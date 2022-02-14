32a64ae1ec81690051ba0507c3bfd45fb1bb94ba
Vinicius Silva <viniciusfabi1@hotmail.com>
2022-02-14 17:48:14 -0300

    First commit with new files

--- flaskproject/src/ext/site/main.py
+++ flaskproject/src/ext/site/main.py
@@ -6,6 +6,14 @@ bp = Blueprint("site", __name__)
 
 @bp.route("/")
 def index():
+    return render_template("index.html")
 
-    return render_template(
-        "index.html")
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
+
+
+@bp.route("/restaurantes")
+def restaurants():
+    return render_template("restaurants.html")
