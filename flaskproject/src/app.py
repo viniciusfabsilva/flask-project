025f4a882cef1757d1884f444e19bf5c75580ec4
Vinicius Silva <viniciusfabi1@hotmail.com>
2022-02-15 23:28:40 -0300

    Criei pastas novas e adicionei uma extensão toolbar do flask para auxiliar em debug/teste

--- flaskproject/src/app.py
+++ flaskproject/src/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from src.ext import site
+from src.ext import config
+from src.ext import toolbar
 
 def create_app():
     app = Flask(__name__)
+    config.init_app(app)
+    toolbar.init_app(app)
     site.init_app(app)
     return app
--- flaskproject/src/ext/site/main.py
+++ flaskproject/src/ext/site/main.py
@@ -1,11 +1,13 @@
 from flask import render_template
 from flask import Blueprint
+from flask import current_app
 
 bp = Blueprint("site", __name__)
 
 
 @bp.route("/")
 def index():
+    #current_app.logger.debug("entrei na função main") << ferramenta de debug para o toolbar
     return render_template("index.html")
 
 
